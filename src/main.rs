eb31f4d005f789e5a31860108e8e0dfa7969032a
zsytssk <zsytssk@gmail.com>
2022-03-30 15:24:37 +0800

    master:

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-#[derive(Debug)]
-enum Num {
-    Int(i32),
-    Float(f64),
-}
-
 fn main() {
-    let mut v = vec![Num::Int(3), Num::Float(1.0)];
+    let s = String::new();
 
-    println!("{:?}", v);
+    println!("{}", s);
 }
